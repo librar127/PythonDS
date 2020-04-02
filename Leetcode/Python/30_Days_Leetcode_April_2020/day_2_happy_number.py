b22a1941696b0e0b720aaa5eb92a5980f6b416b8
Rajneesh <librar127@gmail.com>
2020-04-02 03:58:46 -0400

    Day 2 Happy Number - April LeetCode

---
+++ Leetcode/Python/30_Days_Leetcode_April_2020/day_2_happy_number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isHappy(self, n):
+        seen_set = set()
+        while n > 1:
+            sum = 0
+            while n>0:
+                sum += (n%10)**2
+                n = n//10
+            n = sum
+            
+            if sum in seen_set:
+                return False
+            else:
+                seen_set.add(sum)
+            
+        return True
+            
+s = Solution()
+print(s.isHappy(20))      
