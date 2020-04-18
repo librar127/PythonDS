c5e3cca410af9b58051dc79c5d9002cf537d527f
Rajneesh <librar127@gmail.com>
2020-04-18 04:06:42 -0400

    Maximum Depth

--- Leetcode/Python/Learn_By_Topic/BinaryTree/max_depth.py
+++ Leetcode/Python/Learn_By_Topic/BinaryTree/max_depth.py
@@ -1,7 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     
     # Bottom-Up Recursion    
-    def maxDepth(self, root: TreeNode) -> int:
+    def maxDepth_recursion(self, root: TreeNode) -> int:
         
         if not root:
             return 0
@@ -10,3 +17,29 @@ class Solution:
         right_depth = 1+self.maxDepth(root.right)
         
         return max(left_depth, right_depth)
+    
+    # Iterative Approach - Using Queue   
+    def maxDepth(self, root: TreeNode) -> int:
+        
+        if not root:
+            return 0
+                
+        queue = [root]
+        queue.insert(0, None)
+        max_depth = 0
+        
+        while queue:
+            
+            root = queue.pop()
+            if root:
+                if root.left:
+                    queue.insert(0, root.left)
+                if root.right:
+                    queue.insert(0, root.right)
+            else:
+                max_depth +=1
+                if not queue:
+                    return max_depth
+                else:                
+                    queue.insert(0, None)    
+                
