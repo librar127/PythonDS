b359d1072f36524f48c92c91cd535342ae9abaa9
Rajneesh <librar127@gmail.com>
2020-04-19 21:44:40 -0400

    Day 19: Medium:  Search in Rotated Sorted Array

--- Leetcode/Python/30_Days_Leetcode_April_2020/day_19_search_in_rotated_array.py
+++ Leetcode/Python/30_Days_Leetcode_April_2020/day_19_search_in_rotated_array.py
@@ -1,40 +1,20 @@
 class Solution:
     def search(self, nums, target):
-        
-        # Two Step approach
-        # 1. Find Pivot Element
-        start = 0
-        end = len(nums)-1
-        
-        while start < end:
-            mid = (start + end )//2
-            
-            # This point would be the point of change. From higher to lower value.
-            if nums[mid] > nums[end]:
-                start = mid + 1
-            else:
-                end = mid
-                
-        pivot = start
-    
-
-        start = 0
-        end = len(nums)-1
-        if target < nums[0]:
-            start = pivot
-        else:
-            end = pivot-1
-            
+        start, end = 0, len(nums) - 1
         while start <= end:
-            mid = (start + end ) //2
-            
-            if target == nums[mid]:
+            mid = (start + end)//2
+            if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                start = mid + 1
+            elif nums[start] <= nums[mid]:
+                if nums[start] <= target < nums[mid]:
+                    end = mid -1
+                else:
+                    start = mid + 1
             else:
-                end = mid-1
-                
+                if nums[end] >= target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
         return -1
         
 s = Solution()
@@ -42,6 +22,15 @@ nums = [4,5,6,7,0,1,2]
 print(s.search(nums, 7))
 
 nums = [5,7,8,9,10,11,13,0,1]
+print(s.search(nums, 0))
+
+nums = [3,5,1,2]
+print(s.search(nums, 0))
+
+nums = [2,3,5,1]
+print(s.search(nums, 0))
+
+nums = [1]
 print(s.search(nums, 0))
                 
         
