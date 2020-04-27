cf24e3a6d2f3353aff79f3890205b0ad21ad12f3
Rajneesh <librar127@gmail.com>
2020-04-27 02:03:00 -0400

    Day 26: Medium: Logest Common Subsequence Length

--- Leetcode/Python/30_Days_Leetcode_April_2020/day_26_longest_common_subsequence.py
+++ Leetcode/Python/30_Days_Leetcode_April_2020/day_26_longest_common_subsequence.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestCommonSubsequence(self, text1, text2):
         
         max_len = 0
         arr = []
