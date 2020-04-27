96fd443122b7ae4c911ae4baa2d927923a7a9f24
Rajneesh <librar127@gmail.com>
2020-04-27 02:02:37 -0400

    Day 26: Medium: Logest Common Subsequence Length

---
+++ Leetcode/Python/30_Days_Leetcode_April_2020/day_26_longest_common_subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        max_len = 0
+        arr = []
+        m = len(text1)
+        n = len(text2)
+        for i in range(m+1):
+            arr.append([0]*(n+1))
+   
+        for i in range(0, m):
+            for j in range(0, n):
+                
+                if text1[i] == text2[j]:
+                    arr[i+1][j+1] = arr[i][j] + 1
+                else:
+                    arr[i+1][j+1] = max(arr[i][j+1], arr[i+1][j])
+                    
+        return arr[m][n]
+                
+        
