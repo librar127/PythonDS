7f5562c24a5a09411df17237916b82092017a369
Rajneesh <librar127@gmail.com>
2020-04-29 00:20:44 -0400

    Day 28: Medium: First Unique

---
+++ Leetcode/Python/30_Days_Leetcode_April_2020/day_28_first_unique.py
@@ -0,0 +1,36 @@
+class FirstUnique:
+    
+    def arrange(self):
+        self.hashmap = {}
+        for each in self.nums:
+            if each in self.hashmap:
+                self.hashmap[each] += 1
+            else:                
+                self.hashmap[each] = 1
+        
+    
+    def __init__(self, nums):
+        self.nums = nums
+        self.hashmap = {}
+        self.pointer = 0
+        
+        self.arrange()
+
+    def showFirstUnique(self):
+        if len(self.nums) <= 0:
+            return -1
+        
+        for index in range(self.pointer, len(self.nums)):
+            self.pointer = index
+            if self.hashmap[self.nums[index]] == 1:
+                return self.nums[index]
+            
+        return -1 
+
+    def add(self, value):
+        self.nums.append(value)
+        
+        if value in self.hashmap:
+            self.hashmap[value] += 1
+        else:                
+            self.hashmap[value] = 1
