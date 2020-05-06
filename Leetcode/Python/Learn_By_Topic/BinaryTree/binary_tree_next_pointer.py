1147194fa6af6c31732512af2b9d8fbfe7afc00f
Rajneesh <librar127@gmail.com>
2020-05-05 20:35:08 -0400

    Binary Tree Next Pointer I

--- Leetcode/Python/Learn_By_Topic/BinaryTree/binary_tree_next_pointer.py
+++ Leetcode/Python/Learn_By_Topic/BinaryTree/binary_tree_next_pointer.py
@@ -1,13 +1,42 @@
 # Definition for a Node.
 class Node:
-    def __init__(self, val: int = 0, left = None, right = None, next = None):
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
         self.val = val
         self.left = left
         self.right = right
         self.next = next
 
 class Solution:
+    
+    # Tn = O(N), Sn = O(1)
     def connect(self, root):
+        
+        if not root:
+            return
+        
+        leftmost = root
+        
+        while leftmost.left:
+            
+            
+            head = leftmost            
+            while head:
+                
+                # 1
+                head.left.next = head.right
+                
+                # 2
+                if head.next:
+                    head.right.next = head.next.left
+                    
+                head = head.next
+                
+            leftmost = leftmost.left
+            
+        return root
+    
+    # Tn = O(N), Sn = O(N)
+    def connect_2(self, root):
         if not root:
             return 
         
@@ -27,11 +56,3 @@ class Solution:
                 queue.append(node.right)
         
         return root
-                
-                
-            
-            
-            
-            
-            
-            
