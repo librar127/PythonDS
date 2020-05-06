2c96d8471c643ddcf695f4630bc5011134c5c7ed
Rajneesh <librar127@gmail.com>
2020-05-06 01:36:25 -0400

    LCA of Binary Tree

---
+++ Leetcode/Python/Learn_By_Topic/BinaryTree/lca_of_binary_tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        
+        if not root:
+            return None
+        
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        
+        if left:
+            return left 
+        else:
+            return right
+        
+        
+        
+        
+            
+        
+        
