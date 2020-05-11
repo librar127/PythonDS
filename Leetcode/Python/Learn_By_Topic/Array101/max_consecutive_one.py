480463af396fc3763bff6241b6ed25f60fb47d86
Rajneesh <librar127@gmail.com>
2020-05-11 01:59:09 -0400

    Starting Array 101 Topic

---
+++ Leetcode/Python/Learn_By_Topic/Array101/max_consecutive_one.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        max_count_so_far = 0
+        current_count = 0
+        
+        for index, each in enumerate(nums):
+            if each == 1:
+                current_count += 1
+            else:
+                if current_count > max_count_so_far:
+                    max_count_so_far = current_count
+                current_count = 0
+        if current_count > max_count_so_far:
+            return current_count
+        else:
+            return max_count_so_far
+    
+s  = Solution()
+nums = [1,1,0,1,1,1]
+print(s.findMaxConsecutiveOnes(nums))
