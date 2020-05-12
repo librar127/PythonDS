b8cbd0f5fea4ab585e763b429d20525395c440f0
Rajneesh <librar127@gmail.com>
2020-05-12 00:52:21 -0400

    Remove Duplicates from Sorted Array

--- Leetcode/Python/Learn_By_Topic/Array101/remove_duplicate_from_sorted_array.py
+++ Leetcode/Python/Learn_By_Topic/Array101/remove_duplicate_from_sorted_array.py
@@ -1,3 +1,4 @@
+## https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
 class Solution:
     def removeDuplicates(self, nums):
         
@@ -8,6 +9,20 @@ class Solution:
             else:
                 index += 1
         return len(nums)
+    
+    
+        ## Approach 2 
+        start = 0
+        index = 0
+        while index < len(nums)-1:            
+            if nums[index] != nums[index+1]:
+                nums[start+1] = nums[index+1]
+                start += 1
+                index += 1
+            else:
+                index += 1
+                
+        return start+1
     
 s = Solution()
 nums = [0,0,1,1,1,2,2,3,3,4]
