7928e28f85e544ec150f116b5d9805e73dfd4fc0
Rajneesh <librar127@gmail.com>
2020-05-19 02:08:52 -0400

    Max Consecutive Ones II

--- Leetcode/Python/Learn_By_Topic/Array101/max_consecutive_ones_2.py
+++ Leetcode/Python/Learn_By_Topic/Array101/max_consecutive_ones_2.py
@@ -19,6 +19,19 @@ class Solution:
             high += 1
             
         return max_consecutive_ones
+    
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        # previous and current length of consecutive 1 
+        pre, curr, maxlen = -1, 0, 0
+        for n in nums:
+            if n == 0:
+                pre, curr = curr, 0
+            else:
+                curr += 1
+            maxlen = max(maxlen, pre + 1 + curr )
+        
+        return maxlen
 
 s = Solution()
 nums = [1,0,1,1,0]
