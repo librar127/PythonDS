534ad2dbfc2ad6217c84167e2c19e57121671569
Rajneesh <librar127@gmail.com>
2020-05-24 23:13:37 -0400

    Insert into a Cyclic Sorted List

--- Leetcode/Python/Learn_By_Topic/LinkedList/insert_into_cyclic_sorted_list.py
+++ Leetcode/Python/Learn_By_Topic/LinkedList/insert_into_cyclic_sorted_list.py
@@ -5,6 +5,7 @@ class Node:
         self.val = val
         self.next = next
 
+# Day 1 solution
 class Solution:
     def insert(self, head: 'Node', insertVal: int) -> 'Node':
         
@@ -51,4 +52,44 @@ class Solution:
             prev.next = node
         
         return head
+
+    # Solving it again - day 2
+    def insert(self, head: 'Node', insertVal: int) -> 'Node':
         
+        # Create the node anyways
+        node = Node(insertVal, None)
+        
+        # if head is None
+        if head is None:
+            head = node
+            head.next = head
+            return head
+        
+        prev = head
+        current = head.next
+        inserted = False
+        while prev.next != head:
+            if insertVal >= prev.val and insertVal <= current.val:
+                node.next = current
+                prev.next = node
+                inserted = True
+                break
+            elif prev.val > current.val and insertVal >= prev.val:                
+                node.next = current
+                prev.next = node
+                inserted = True
+                break
+            elif prev.val > current.val and insertVal <= current.val:                
+                node.next = current
+                prev.next = node
+                inserted = True
+                break
+                
+            prev = prev.next
+            current = current.next
+            
+        if not inserted:
+            node.next = current
+            prev.next = node
+        
+        return head
