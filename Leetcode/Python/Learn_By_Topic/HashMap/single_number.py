33a2f911f0e2d50affb033f83cb13ed95b1d7fae
Rajneesh <librar127@gmail.com>
2020-05-25 02:08:39 -0400

    Single Number

---
+++ Leetcode/Python/Learn_By_Topic/HashMap/single_number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def singleNumber(self, nums):
+        
+        # Solution 1
+        hashset = set(nums)
+        sum = 0
+        for each in hashset:
+            sum += each * 2
+
+        for each in nums:
+            sum -= each
+
+        return sum
+    
+        # Solution 2
+        xor = 0
+        
+        for each in nums:
+            xor ^= each
+        return xor
