7768b6e6ababf91f17ec62650de8825a1627c916
Rajneesh <librar127@gmail.com>
2020-05-25 03:09:30 -0400

    Isomorphic Strings

---
+++ Leetcode/Python/Learn_By_Topic/HashMap/isomorphic_strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        hashmap = {}
+        hashset = set()
+        
+        for index, each in enumerate(s):
+            if each in hashmap:
+                if t[index] != hashmap[each]:
+                    return False  
+            elif t[index] in hashset:
+                return False
+            
+            else:
+                hashmap[each] = t[index]   
+                hashset.add(t[index])
+        
+        return Trues
