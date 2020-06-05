4a53a4e6b036dbfe363d3bc886365d6d0e69bbc2
Rajneesh <librar127@gmail.com>
2020-06-05 01:15:38 -0400

    Group Anagrams

---
+++ Leetcode/Python/Learn_By_Topic/HashMap/group_anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs):    
+    
+        hashmaps = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+
+            if sorted_word in hashmaps:
+                hashmaps.get(sorted_word).append(word)
+            else:
+                hashmaps[sorted_word] = [word]
+
+        return list(hashmaps.values())
+    
+s = Solution()
+input = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(s.groupAnagrams(input))
