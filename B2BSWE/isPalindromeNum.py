fd8dc6aa02a23c0b6dc56a8af64706dc97854f6a
Rajneesh <librar127@gmail.com>
2020-12-16 00:10:04 -0500

    isPalindrome an Integer

--- B2BSWE/isPalindromeNum.py
+++ B2BSWE/isPalindromeNum.py
@@ -1,4 +1,6 @@
 
+import math
+
 class Solution:
     def isPalindrome(self, x):
         '''
@@ -22,7 +24,30 @@ class Solution:
                 return True
             else:
                 return False
+                
+    def isPalindrome(self, x):
+        '''
+        :type x: int
+        :rtype: bool
+        '''
+        
+        if x < 0:
+            return False
+        elif x%10 == x:
+            return True
+        else:
+            num_digits = math.floor(math.log10(x)) + 1 
+            for each in range(num_digits//2):
+                print(each, x, x // 10**(num_digits-(each*2+1)), x % 10)
+                if x // 10**(num_digits-(each*2+1)) == x % 10:                    
+                    x %= 10**(num_digits-(each*2+1))
+                    x //= 10
+                    print(x)
+                else:
+                    return False
+                    
+        return True
             
 s = Solution()
-ans = s.isPalindrome(323)
+ans = s.isPalindrome(32)
 print(ans)
