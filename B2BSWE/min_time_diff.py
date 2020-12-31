ad107ef9c610943fe89fa41afcf67261d852f725
Rajneesh <librar127@gmail.com>
2020-12-30 22:30:11 -0500

    Adding Another Optimized solution approach

--- B2BSWE/min_time_diff.py
+++ B2BSWE/min_time_diff.py
@@ -1,13 +1,14 @@
 
 class Solution:
-    def timeDifference(self, times):
+    def timeDifference_1(self, times):
         '''
         :type times: list of str
         :rtype: int
+        :O(NLogN) solution
         '''
         min_diff = 999999
         s_times = sorted(times)
-        print(s_times)
+        
         for index in range(len(s_times)-1):
             next_a, next_b = s_times[index+1].split(":")
             curr_a, curr_b = s_times[index].split(":")
@@ -26,6 +27,42 @@ class Solution:
                 
         return min_diff
         
+    def timeDifference(self, times):
+        '''
+        :type times: list of str
+        :rtype: int
+        :O(N) solution
+        '''
+        min_diff = 9999
+        min_list = [0]*1440
+        
+        for each_time in times:
+            hh,mm = each_time.split(":")
+            time_2_index = int(hh)*60+int(mm)
             
-s = Solution()
-print(s.timeDifference(["00:03","23:59","12:03"]))
+            if min_list[time_2_index]:
+                return 0
+            
+            min_list[time_2_index] = time_2_index
+            
+        start_flag = False
+        last_index = 0
+        for each in min_list:
+            if each > 0:
+                last_index = each
+                
+        for index,each in enumerate(min_list):
+            if each > 0:
+                
+                if start_flag == False:
+                    diff = each+1440 - min_list[last_index]
+                else:
+                    diff = each - min_list[last_index]
+                    
+                start_flag = True   
+                if diff < min_diff:
+                    min_diff = diff
+                    
+                last_index = index
+        
+        return min_diff
