af91f75c91ac5f69eea7382209e573c2a8628b11
Rajneesh <librar127@gmail.com>
2021-02-09 22:25:19 -0500

    Postgres Aggregates

--- DEInterview/SQL/postgres_ggregations.sql
+++ DEInterview/SQL/postgres_ggregations.sql
@@ -56,3 +56,18 @@ join cd.bookings b
 on m.memid = b.memid
 group by m.firstname, m.surname
 order by rank, m.surname, m.firstname
+
+
+-- 6. Find the top three revenue generating facilities
+select name, rank from (
+	select facs.name as name, rank() over (order by sum(case
+				when memid = 0 then slots * facs.guestcost
+				else slots * membercost
+			end) desc) as rank
+		from cd.bookings bks
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+		group by facs.name
+	) as subq
+	where rank <= 3
+order by rank;  
