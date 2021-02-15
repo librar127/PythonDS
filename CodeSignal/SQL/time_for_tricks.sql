105084d7e5a6a22cc2c23977e8e0e25955133f68
Rajneesh <librar127@gmail.com>
2021-02-14 21:49:55 -0500

    NULLInTern

--- CodeSignal/SQL/time_for_tricks.sql
+++ CodeSignal/SQL/time_for_tricks.sql
@@ -4,3 +4,10 @@ FROM users
 WHERE type='user'
 OR type !='user'
 ORDER BY id
+
+-- NullIntern
+SELECT count(*) number_of_nulls FROM departments
+WHERE DESCRIPTION is NULL
+OR UPPER(trim(DESCRIPTION)) = 'NULL'
+OR trim(DESCRIPTION) = '-'
+OR UPPER(trim(DESCRIPTION)) = 'NIL';
