65b4d09381c67eb40f475448afdda96180aa92cd
Rajneesh <librar127@gmail.com>
2021-02-23 00:36:23 -0500

    Match Winner

--- CodeSignal/SQL/match_winner.sql
+++ CodeSignal/SQL/match_winner.sql
@@ -27,29 +27,34 @@ END
 
 --- 7/8 Test cases passed
 
---- Someone else's code
-/*Please add ; after each select statement*/
-CREATE PROCEDURE soccerGameSeries()
+--- Second Attempt Below
+
+-CREATE PROCEDURE soccerGameSeries()
 BEGIN
-    SELECT
-        CASE
-            WHEN stats.`first_team_wins` > stats.`second_team_wins` THEN 1
-            WHEN stats.`second_team_wins` > stats.`first_team_wins` THEN 2
-            WHEN stats.`goal_diff` > 0 THEN 1
-            WHEN stats.`goal_diff` < 0 THEN 2
-            WHEN stats.`first_team_points` > stats.`second_team_points` THEN 1
-            WHEN stats.`second_team_points` > stats.`first_team_points` THEN 2
-            ELSE 0
-        END AS `winner`
-    FROM
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+     with nT as 
     (
-        SELECT
-            SUM(scores.`first_team_score` > scores.`second_team_score`) `first_team_wins`,
-            SUM(scores.`first_team_score` < scores.`second_team_score`) `second_team_wins`,
-            SUM(scores.`first_team_score` - scores.`second_team_score`) `goal_diff`,
-            SUM((scores.`match_host` = 2) * scores.`first_team_score`) `first_team_points`,
-            SUM((scores.`match_host` = 1) * scores.`second_team_score`) `second_team_points`
-        FROM
+        select
+            sum(case when first_team_score > second_team_score then 1 
+                when first_team_score < second_team_score then -1 else 0 end) as diff_score,
+            sum(first_team_score) as total_first,
+            sum(second_team_score) as total_second,
+            sum(case when match_host = 2 then first_team_score
+                     when match_host = 1 then -1*second_team_score
+                     else 0 end) as home_away_score            
+        from 
             scores
-    ) AS stats;
+    )
+    select 
+        case 
+            when diff_score > 0 then 1 
+            when diff_score < 0 then 2
+            when diff_score = 0 and total_first > total_second then 1 
+            when diff_score = 0 and total_first < total_second then 2
+            when diff_score = 0 and total_first = total_second and home_away_score > 0 then 1
+            when diff_score = 0 and total_first = total_second and home_away_score < 0 then 2
+            else 0 
+        end as winner
+    from
+        nT;    
 END
