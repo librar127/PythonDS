c0222f42ca704821fe8b04c1d5742ee7bde75f67
Rajneesh <librar127@gmail.com>
2021-02-23 00:15:24 -0500

    Match Winner

---
+++ CodeSignal/SQL/match_winner.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE soccerGameSeries()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+     with nT as 
+    (
+        select
+            sum(case when first_team_score-second_team_score > 0 then 1 
+                when first_team_score-second_team_score < 0 then -1 else 0 end) as diff_score,
+            sum(first_team_score) as total_first,
+            sum(second_team_score) as total_second,
+            sum(case when first_team_score-second_team_score > 0 and match_host=2 then first_team_score
+                     when second_team_score-first_team_score > 0 and match_host=1 then -1*second_team_score
+                     else 0 end) as frequent_host            
+        from 
+            scores
+    )
+    select 
+        case when diff_score > 0 then 1 
+        when diff_score < 0 then 2
+        when diff_score = 0 and (total_first - total_second) > 0 then 1 
+        when diff_score = 0 and (total_first - total_second) < 0 then 2
+        when diff_score = 0 and (total_first - total_second) = 0 and frequent_host > 0 then 1
+        when diff_score = 0 and (total_first - total_second) = 0 and frequent_host < 0 then 2 else 0 end as winner
+    from
+        nT;    
+END
+
+--- 7/8 Test cases passed
