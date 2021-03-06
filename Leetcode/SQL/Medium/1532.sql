9da3a405ba553869e88a507a055c92aeab532cd7
Rajneesh <librar127@gmail.com>
2021-03-06 15:16:37 -0500

    1532. The Most Recent Three Orders

---
+++ Leetcode/SQL/Medium/1532.sql
@@ -0,0 +1,31 @@
+-- Write your MySQL query statement below
+
+WITH nT AS
+(
+    SELECT
+        A.name customer_name,
+        A.customer_id,
+        B.order_id,
+        B.order_date,
+        ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date DESC) rn
+    FROM
+        Customers A
+    JOIN
+        Orders B
+    ON
+        A.customer_id = B.customer_id
+)
+
+SELECT
+    customer_name,
+    customer_id,
+    order_id,
+    order_date
+FROM
+    nT
+WHERE 
+    rn < 4
+ORDER BY
+    customer_name, 
+    customer_id
+    
