76ec57d75c1252f5f826a60ba44e7df1f054626d
Rajneesh <librar127@gmail.com>
2021-03-07 15:45:10 -0500

    1158. Market Analysis I

--- Leetcode/SQL/Medium/1158.sql
+++ Leetcode/SQL/Medium/1158.sql
@@ -28,3 +28,18 @@ GROUp BY
     user_id,
     join_date
     
+
+-- ANSWER 2
+SELECT
+    A.user_id buyer_id,
+    A.join_date,
+    SUM(CASE WHEN B.order_date IS NOT NULL AND YEAR(B.order_date) = 2019 THEN 1 ELSE 0 END)orders_in_2019
+FROM
+    Users A
+LEFT JOIN
+    Orders B
+ON 
+    A.user_id = B.buyer_id
+GROUP BY
+    A.user_id,
+    A.join_date
