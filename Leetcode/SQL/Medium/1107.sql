b1f1380bd0cca05210efa0c49cd48d346f0346ff
Rajneesh <librar127@gmail.com>
2021-03-08 02:13:57 -0500

    1107. New Users Daily Count

--- Leetcode/SQL/Medium/1107.sql
+++ Leetcode/SQL/Medium/1107.sql
@@ -21,3 +21,28 @@ WHERE
     datediff('2019-06-30', activity_date) <= 90
 GROUP BY
     activity_date
+
+
+-- Write your MySQL query statement below
+WITH nT as
+(
+    SELECT
+        user_id,
+        min(activity_date) activity_date
+    FROM
+        Traffic
+    WHERE
+        activity = 'login'
+    GROUP BY
+        user_id
+)
+
+SELECT
+    activity_date login_date,
+    COUNT(distinct user_id) user_count
+FROM
+    nT
+WHERE    
+    activity_date BETWEEN date_sub("2019-06-30", INTERVAL 90 DAY) AND "2019-06-30"  
+GROUP BY
+    activity_date
