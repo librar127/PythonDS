0b70ed07283362b895073d951e58c1493df9dcec
Rajneesh <librar127@gmail.com>
2021-03-08 01:00:03 -0500

    580. Count Student Number in Departments

---
+++ Leetcode/SQL/Medium/580.sql
@@ -0,0 +1,15 @@
+-- Write your MySQL query statement below
+SELECT
+    A.dept_name,
+    COUNT(B.student_id) student_number
+FROM
+    department A
+LEFT JOIN
+    student B
+ON
+    A.dept_id = B.dept_id
+GROUP BY
+    A.dept_name
+ORDER BY
+    student_number DESC,
+    dept_name
