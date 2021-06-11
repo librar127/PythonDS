b294b878cc08b5ff600bfb7d2827b5914d20b6af
Rajneesh <librar127@gmail.com>
2021-06-10 23:31:35 -0400

    1741. Find Total Time Spent by Each Employee

---
+++ Leetcode/SQL/Easy/1741.sql
@@ -0,0 +1,12 @@
+-- Write your MySQL query statement below
+
+SELECT
+    event_day day,
+    emp_id,
+    sum(out_time - in_time)total_time
+FROM
+    Employees 
+GROUP BY
+    event_day,
+    emp_id
+    
